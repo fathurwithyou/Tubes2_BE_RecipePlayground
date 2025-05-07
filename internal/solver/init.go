78da9a45680c5a6305341a2dddf3829380f10c67
Ellipsis <rizkyfathur326@gmail.com>
2025-05-08 02:22:27 +0700

    REFACTOR initialization element

---
+++ internal/solver/init.go
@@ -0,0 +1,14 @@
+package recipetree
+
+import (
+	"Tubes2_BE_RecipePlayground/internal/model"
+)
+
+var elementsMapGlobal map[string]model.Element
+
+func InitElementsMap(data model.Data) {
+	elementsMapGlobal = make(map[string]model.Element)
+	for _, el := range data.Elements {
+		elementsMapGlobal[el.Name] = el
+	}
+}
